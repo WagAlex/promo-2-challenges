166b8ea309cde07575f97dc384b4ac9f6e409e43
WagAlex <alexandretournaud@gmail.com>
2014-04-09 00:11:45 +0200

    f

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,16 @@
 def coach_answer(your_message)
-  # TODO: return coach answer to your_message
+  if your_message == "I am going to work right now SIR !"
+    ""
+
+ elsif your_message.include? "?"
+    "Silly question, get dressed and go to work !"
+
+  else coach_answer = "I don't care son, get dressed and go to work !"
+
 end
 
 def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours !
+
+
 end
 
