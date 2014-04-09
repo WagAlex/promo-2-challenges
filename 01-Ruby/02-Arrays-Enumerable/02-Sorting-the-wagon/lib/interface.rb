564a48510aee233f3c93ce4ffd6477f7328925f6
WagAlex <alexandretournaud@gmail.com>
2014-04-09 15:29:06 +0200

    fr

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -8,15 +8,17 @@ begin
   name.chomp! if name
 
   # TODO: Add the name we just gotto the students array
-
-  students << name
+  if name != ""
+    students << name # students.push(name)
+  end
 
 end while name != ""
 
 # TODO: Call `wagon_sort` method and display the sorted student list
 
+
 list_of_students = wagon_sort(students)
-nb_students = students.lenght - 1
+nb_students = students.length
 
 puts "Congratulations! Your Wagon has #{nb_students} students:"
 
