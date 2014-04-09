b393a55d189c1fb574c8a6d694b6ecae8945e7b4
WagAlex <alexandretournaud@gmail.com>
2014-04-09 22:44:35 +0200

    dd

--- 01-Ruby/02-Arrays-Enumerable/05-Debug-Gym/lib/bugged_concatenate.rb
+++ 01-Ruby/02-Arrays-Enumerable/05-Debug-Gym/lib/bugged_concatenate.rb
@@ -2,6 +2,6 @@ def bugged_concatenate(array)
   array.inject("") { |output, element| output + element }.upcase
 end
 
-def build_1984_title
+def build_1984_title (building)
   bugged_concatenate(1, "9", 84, " ", "George Orwell")
 end
