b03cc76df72f1917abb58fe322a4abe85b311286
WagAlex <alexandretournaud@gmail.com>
2014-04-09 16:50:21 +0200

    cucu

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -2,16 +2,28 @@ require_relative 'black_jack'
 
 def play_game
   #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+score = 0
+
+while score <= bank
+  asking_for_card?(score)
+
 end
 
 def state_of_the_game(score, bank)
   # TODO: Returns custom message with player's score and bank's score
+  puts "Player your score is #{score} "
+  puts "Biatch Bank your score is #{bank}"
 end
 
 def asking_for_card?(score)
   # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
+  if bank_score <= 21
+    puts true
+  else
+    puts false
 end
 
 def build_message_for(outcome)
   #TODO: return specific message depending on the game outcome (= bank's score and user's score)
+
 end
