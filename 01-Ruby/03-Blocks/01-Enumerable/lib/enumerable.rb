edf4356a900480bd4b9de8cc3b079950de6ecb69
WagAlex <alexandretournaud@gmail.com>
2014-04-10 11:22:25 +0200

    dd

--- 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
+++ 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
@@ -1,9 +1,11 @@
 def sum_odd_indexed(array)
   # TODO: computes the sum of elements at odd indices (1, 3, 5, 7, etc.)
   #       You should make use Enumerable#each_with_index
+  new_array = array.select { |num|  num.odd?  }
   sum = 0
-  array.each_with_index {|s, i| sum += s.to_i if i % 2 == 1}
+  new_array.each { |a| sum+=a }
   puts sum
+
 end
 
 def even_numbers(array)
@@ -15,6 +17,7 @@ end
 def short_words(array, max_length)
   # TODO: Take and array of words, return the array of words not exceeding max_length characters
   #       You should use Enumerable#reject
+  array.reject { |words| words.length < 4 }
 end
 
 def first_under(array, limit)
@@ -25,6 +28,7 @@ end
 def add_bang(array)
   # TODO: Take an array of strings and return a new array with "!" appended to each string.
   #       You should use Enumerable#map
+  array.map! {|word| word + "!"}
 end
 
 def product(array)
