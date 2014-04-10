f890edef250eddef368c7f49caa3f63a6bef4713
WagAlex <alexandretournaud@gmail.com>
2014-04-10 15:18:19 +0200

    rt

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,16 +1,20 @@
 def tag(tag_name, attr = nil)
   #TODO:  Build HTML tags around  content given in the block
   #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
-  tag("h1") { "Les tomates sont rouges" }
+  tag("h1") { "Some Title" }
 
-end
+  end
 
 def timer_for
   #TODO:  Return time taken to execute the given block
-  timer_for do
-  (1..100).each { |i| (1..100000).to_a.shuffle.sort }
+  go_execution = Time.now
+  yield
+  over_execution = Time.now
+
+  over_execution - go_execution
 end
 
 def transform(element)
   #TODO:  Simply execute the given block on element
+  yield(element)
 end
