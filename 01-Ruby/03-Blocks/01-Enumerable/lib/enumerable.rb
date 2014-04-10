26dbe97bbbabfa8369677af9cd6c56dcde5f15b1
WagAlex <alexandretournaud@gmail.com>
2014-04-10 11:34:08 +0200

    ee

--- 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
+++ 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
@@ -1,17 +1,12 @@
 def sum_odd_indexed(array)
   # TODO: computes the sum of elements at odd indices (1, 3, 5, 7, etc.)
   #       You should make use Enumerable#each_with_index
-  new_array = array.select { |num|  num.odd?  }
-  sum = 0
-  new_array.each { |a| sum+=a }
-  puts sum
 
 end
 
 def even_numbers(array)
-  # TODO: Return the even numbers from a list of integers.
-  #       You should use Enumerable#select
-  array.select { |num|  num.even?  }
+  new_array = array.select { |num|  num.even?  }
+
 end
 
 def short_words(array, max_length)
@@ -26,19 +21,17 @@ def first_under(array, limit)
 end
 
 def add_bang(array)
-  # TODO: Take an array of strings and return a new array with "!" appended to each string.
-  #       You should use Enumerable#map
   array.map! {|word| word + "!"}
 end
 
 def product(array)
-  # TODO: Calculate the product of an array of numbers.
-  #       You should use of Enumerable#reduce
+  array.reduce(1, :*)
 end
 
 def sorted_pairs(array)
   # TODO: Reorganize an array into slices of 2 elements, and sort each slice alphabetically.
   #       You should make use of Enumerable#each_slice
+  array.each_slice(2).to_a
 end
 
 
