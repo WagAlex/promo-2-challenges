1e87cdfbc954b08d14111f12578ef5caddf977c3
WagAlex <alexandretournaud@gmail.com>
2014-04-15 16:11:34 +0200

    chien_niche

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,8 +1,16 @@
 def anagrams?(a_string, another_string)
   #TODO: implement the obvious method to test if two words are anagrams
+  word_one = a_string.gsub(/\W/, "").downcase.split(//).sort
+  word_two = another_string.gsub(/\W/, "").downcase.split(//).sort
+
+  word_one == word_two
+
 end
+
+anagrams?("yo!! 8", "!!8    yo")
+
+
 
 def anagrams_on_steroids?(a_string, another_string)
   #TODO: implement the improved method
 end
-  
