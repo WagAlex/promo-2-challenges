d9ca0f573b724702e2be149bb95c11c4f16c574f
WagAlex <alexandretournaud@gmail.com>
2014-04-16 12:18:42 +0200

    petitlapin

--- 02-OOP/02-Getters-and-Setters/lib/playboy.rb
+++ 02-OOP/02-Getters-and-Setters/lib/playboy.rb
@@ -1,5 +1,7 @@
 class Playboy
-
+  attr_reader :nationality
+  attr_accessor :hair_length
+  attr_reader :conquests
   #TODO: implement necessary getters or setters to run meeting_casanova.rb
 
   INITIAL_HAIR_LENGTH = 20  # beautiful long playboy hair
