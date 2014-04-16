9259b930c1724cc28c59f2159861532f450dac99
WagAlex <alexandretournaud@gmail.com>
2014-04-16 16:07:54 +0200

    quiavolélorangedemonarbre

--- 02-OOP/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,42 @@
 class OrangeTree
   # TODO: Implement all the specs defined in the README.md :)
+ attr_accessor :age, :height, :fruits, :dead
+
+
+  def initialize
+    @age = 0
+    @height = 0
+    @fruits = 0
+    @dead = false
+  end
+
+  def one_year_passes!
+    @age += 1
+
+    @height += 1 if @age < 10
+    if 5 <= @age && @age < 10
+      @fruits = 100
+    elsif 10 <= @age && @age < 15
+      @fruits = 200
+    end
+  end
+
+  def dead?
+    if @age < 50
+      @alive = true
+    elsif @age >= 100
+      @alive = false
+    else @age += Random.new.rand(0..50)
+      @alive = false
+    end
+  end
+
+
+  def pick_a_fruit!
+    @fruits -= 1
+  end
+
+
+
+
 end
