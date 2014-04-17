3affa02e12ce14c0746356c7776781b0c39910f7
WagAlex <alexandretournaud@gmail.com>
2014-04-17 17:58:36 +0200

    ddd

--- 02-OOP/06-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/06-Instance-vs-Class/lib/restaurant.rb
@@ -1,33 +1,35 @@
 class Restaurant
 
   #TODO add relevant accessors if necessary
-  attr_accessor = :name, :address, :average_rating
+  attr_accessor :name, :city, :average_rating, :number_rating, :restos_filtered
 
-  def initialize(name, address, average_rating, city)
+  def initialize(name, city)
     #TODO: implement constructor with relevant instance variables
     @name = name
-    @address = address
-    @average_rating = average_rating
+    @average_rating = 0
+    @number_rating = 0
     @city = city
+    @resto = []
+  end
+
+
+  def rate(rating)
+    @number_rating += 1
+    @average_rating = ((rating + (@average_rating * (@number_rating - 1))) / @number_ratin)
+  end
+
+  def self.filter_by_city(restos,city)
+    restos_filtered = []
+    Restaurant.resto each do |restos|
+      if restos.city == city
+        restos_filtered << resto
+      end
+      end
+    restos_filtered
   end
 
 end
 
-class FilterByCity < Restaurant
 
-  attr_writer = :city
 
-  def initialize (city)
-    super(name, address, average_rating)
-    @city = city
-  end
-
-end
-
-  def all_restaurant_given_city
-    restaurants = []
-    restaurant = Restaurant.new
-
-    restaurants << restaurant.city
-  end
 
