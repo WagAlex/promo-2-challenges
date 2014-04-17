28f0cb7aca958099512aad0127a1e36afbd60c6e
WagAlex <alexandretournaud@gmail.com>
2014-04-17 12:00:26 +0200

    biker

--- 02-OOP/05-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/05-Dessert-inheritance/lib/dessert.rb
@@ -1,4 +1,5 @@
 class Dessert
+  attr_writer :name, :calories
   def initialize(name, calories)
     @name = name
     @calories = calories
@@ -18,15 +19,17 @@ class Dessert
 end
 
 class JellyBean < Dessert
+  attr_reader :flavor
   def initialize(name, calories, flavor)
     @flavor = flavor
+    super(name, calories)
   end
 
   def delicious?
-    if @flavor = "black licorice"
+    if @flavor == "black licorice"
       false
     else
-      super
+      true
     end
   end
 end
