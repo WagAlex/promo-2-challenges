a03f47a623e6d9d9f968f58c084049984e43318a
WagAlex <alexandretournaud@gmail.com>
2014-04-21 09:34:41 +0200

    eoe

--- 02-OOP/08-Cookbook/lib/cookbook.rb
+++ 02-OOP/08-Cookbook/lib/cookbook.rb
@@ -1,7 +1,40 @@
+require "csv"
+
 class Cookbook
+
+  attr_reader :recipes
+
+
   def initialize(file)
     # TODO: Retrieve the data from your CSV file and store it in an instance variable
+    load_csv(file)
+    @file = file
   end
+
+  def retrieve_all_recipes
+    @recipes.each_with_index do |index, item|
+      "#{index + 1} + #{item}"
+    end
+  end
+
+
+  def create(name)
+  end
+
+  def destroy
+  end
+
+
+
+  private
+
+  def load_csv(file)
+    @recipes =[]
+    CSV.foreach(file) do |row|
+      @recipes << row.first
+    end
+  end
+
 
   # TODO: Implement the methods to retrieve all recipes, create, or destroy recipes
   # TODO: Implement a save method that will write the data into the CSV
